c77493054b115347a3c00fb3312ddfa8a267801a
Raghav Mehra <ragmehra@hotmail.com>
2026-07-27 10:16:36 -0700

    Complete coke.py

--- PS2/coke/coke.py
+++ PS2/coke/coke.py
@@ -1,5 +1,18 @@
 def main(): 
-    ...
+    due = 50
+    while due > 0: 
+        print(f"Amount Due: {due}")
+        insert = int(input("Insert Coin: "))
+        if insert == 0: 
+            print("Not a valid amount")
+            continue
+        elif insert == 25 or insert == 10 or insert == 5:
+            due = due - insert
+        else: 
+            print("Not a valid amount")
+
+    change =  due * -1
+    print(f"Change Owed: {change}") 
 
 
 
