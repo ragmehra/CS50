4c132045bc6f8b939d5fddde16e46ce78cabdf5b
Raghav Mehra <ragmehra@hotmail.com>
2026-07-27 10:32:53 -0700

    Complete twttr.py

--- PS2/twttr/twttr.py
+++ PS2/twttr/twttr.py
@@ -1,7 +1,17 @@
 def main(): 
-    ... 
+    user_input = input("Word: ")
+    print(remove_vowels(user_input))
 
 
+def remove_vowels(word): 
+    result = ''
+    for l in word: 
+        if l in "aeiouAEIOU": 
+            continue
+        else: 
+            result = result + l
+
+    return result
 
 
 if __name__ == "__main__": 
