8ac517401b9c150452b2e1700f37f3c3c81423c6
Raghav Mehra <ragmehra@hotmail.com>
2026-08-03 10:29:25 -0700

    Add more tests to test_twttr.py

--- PS2/twttr/test_twttr.py
+++ PS2/twttr/test_twttr.py
@@ -3,5 +3,5 @@ from twttr import shorten
 def test_shorten():
     assert shorten("twitter") == 'twttr'
     assert shorten("goobie") == "gb"
-
-    
+    assert shorten("twItter") == "twttr"
+    assert shorten("123,a") == "123,"
